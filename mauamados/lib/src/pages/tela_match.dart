2bd91f00007afd7a235e107047fc4d2006ddb74b
Eduardo <eduardoc.santiago@hotmail.com>
2023-11-30 17:57:30 -0300

    tela match finalizada, falta integrar

--- mauamados/lib/src/pages/tela_match.dart
+++ mauamados/lib/src/pages/tela_match.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class TelaMatch extends StatefulWidget {
   final String link;
+
   const TelaMatch({
     required this.link,
     Key? key,
@@ -28,6 +29,7 @@ class _TelaMatchState extends State<TelaMatch> {
   Widget build(BuildContext context) {
     double deviceHeight = MediaQuery.of(context).size.height;
     double deviceWidth = MediaQuery.of(context).size.width;
+    double deviceSize = MediaQuery.of(context).size.shortestSide * 0.02;
     double fotoHeight = deviceHeight;
     double fotoWidth = deviceHeight;
 
@@ -39,25 +41,58 @@ class _TelaMatchState extends State<TelaMatch> {
       fotoHeight = deviceHeight;
     }
 
-    return Container(
-      width: deviceWidth,
-      height: deviceHeight,
-      color: Colors.black,
-      child: Center(
-        child: Container(
-          height: fotoHeight,
-          width: fotoWidth,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(16.0),
-            child: Image(
-              image: NetworkImage(
-                widget.link,
+    return Scaffold(
+      body: Container(
+        width: deviceWidth,
+        height: deviceHeight,
+        color: Colors.black,
+        child: Center(
+          child: Stack(
+            children: [
+              SizedBox(
+                height: fotoHeight,
+                width: fotoWidth,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16.0),
+                  child: Image(
+                    image: NetworkImage(
+                      widget.link,
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
-              fit: BoxFit.cover,
-            ),
+              Positioned(
+                right: 0,
+                child: Padding(
+                  padding: EdgeInsets.all(deviceSize * 5),
+                  child:   Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 3,
+                        color: Colors.green
+                      ),
+                      borderRadius: BorderRadius.circular(15)
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(deviceSize),
+                      child: Text(
+                        'MATCH',
+                        style: TextStyle(
+                          fontSize: deviceSize * 5,
+                          color: Colors.green,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    )
+                  ),
+                ),
+              )
+            ],
           ),
         ),
       ),
     );
+    
   }
 }
